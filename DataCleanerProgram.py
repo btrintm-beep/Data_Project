117ec9e75614fc8d7544ff2055676f7464dd78c5
btrintm-beep <btrintm@gmail.com>
2026-03-18 16:08:00 -0600

    Fix typo

--- DataCleanerProgram.py
+++ DataCleanerProgram.py
@@ -4,7 +4,7 @@ import numpy as np
 
 #Loading the raw data
 
-df = pd.read_csav("cardio_train.csv", sep = ";")
+df = pd.read_csv("cardio_train.csv", sep = ";")
 
 print(f"Raw dataset shape: {df.shape}")
 print(f"Raw dataset columns: {df.columns}\n")
