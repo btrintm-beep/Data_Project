1d7837c07c230a3dfdc1fd60ae0e703affaa5dee
btrintm-beep <btrintm@gmail.com>
2026-03-22 12:35:21 -0600

    Fix Typo for columns on NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -8,7 +8,8 @@ from tensorflow import keras
 
 df = pd.read_csv("cardio_clean.csv")
 
-X = df.drop(["cardio"])
+
+X = df.drop(columns=["cardio"])
 Y = df["cardio"]
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42, stratify = Y)
