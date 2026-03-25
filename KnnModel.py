6430c60c66e0c95282ac5a2f4b5472b3a9937aae
Adoneesk <adonees.kawash@ucalgary.ca>
2026-03-25 11:06:09 -0600

    Add imorts and split cleaned data using train_test_split

---
+++ KnnModel.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+import joblib
+
+data_frame = pd.read_csv("cardio_clean.csv")
+
+features = data_frame.drop(columns=["cardio"])
+labels = data_frame["cardio"]
+
+features_train, features_test, labes_train, labels_test = train_test_split(features, labels, test_size=0.2, random_state=42, stratify=labels)
